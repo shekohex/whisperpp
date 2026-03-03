94fd938e4cc3796563d254b128929a32a1894ff7
Shady Khalifa <dev+github@shadykhalifa.me>
2026-03-03 18:52:10 +0000

    feat(03-01): add KeyboardState helper flags
    - Add isRecording/isPaused/isLocked helpers for dictation UI rendering

--- android/app/src/main/java/com/github/shekohex/whisperpp/keyboard/KeyboardState.kt
+++ android/app/src/main/java/com/github/shekohex/whisperpp/keyboard/KeyboardState.kt
@@ -9,3 +9,12 @@ enum class KeyboardState {
     Transcribing,
     SmartFixing,
 }
+
+val KeyboardState.isRecording: Boolean
+    get() = this == KeyboardState.Recording || this == KeyboardState.RecordingLocked
+
+val KeyboardState.isPaused: Boolean
+    get() = this == KeyboardState.Paused || this == KeyboardState.PausedLocked
+
+val KeyboardState.isLocked: Boolean
+    get() = this == KeyboardState.RecordingLocked || this == KeyboardState.PausedLocked
